f0fc3bd0de94a9e30e313d296d22bde6fc33a39f
Anton Salauyeu <solowks+rsschool@gmail.com>
2023-02-12 16:57:44 +0100

    task is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function toReadable (number) {
-  
+const humanReadable = [
+    ['zero', 'ten', ''],
+    ['one', 'eleven', ''],
+    ['two', 'twelve', 'twenty'],
+    ['three', 'thirteen', 'thirty'],
+    ['four', 'fourteen', 'forty'],
+    ['five', 'fifteen', 'fifty'],
+    ['six', 'sixteen', 'sixty'],
+    ['seven', 'seventeen', 'seventy'],
+    ['eight', 'eighteen', 'eighty'],
+    ['nine', 'nineteen', 'ninety'],
+];
+
+module.exports = function toReadable(number) {
+    if (number < 10) {
+        return humanReadable[number][0];
+    }
+    let result = '';
+    let analizedNumber = number;
+    let hundreds = Math.floor(analizedNumber / 100);
+    if (hundreds > 0) {
+        result += humanReadable[hundreds][0] + ' hundred ';
+        analizedNumber = analizedNumber % 100;
+    }
+
+    let decades = Math.floor(analizedNumber / 10);
+    let simples = analizedNumber % 10;
+    if (decades == 1) {
+        result += humanReadable[simples][1];
+    } else {
+        result += humanReadable[decades][2];
+        if (simples != 0) {
+            result = result.trimEnd() + ' ' + humanReadable[simples][0];
+        }
+    }
+    return result.trimEnd();
 }
